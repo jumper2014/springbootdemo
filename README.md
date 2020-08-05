c6d01c5af2e4ba9481cbb0f2f0667d5ec8253748
zyt <ijumper@163.com>
2020-08-05 10:38:44 +0800

    integrate with thymeleaf

---
+++ README.md
@@ -0,0 +1,9 @@
+# Demo to study spring boot framework
+
+
+### History
+- spring boot 
+- mybatis
+- log4j2
+- Thymeleaf
+
--- src/main/java/com/zyt/demo/springbootdemo/controller/UserController.java
+++ src/main/java/com/zyt/demo/springbootdemo/controller/UserController.java
@@ -8,6 +8,14 @@ import java.util.List;
 import com.zyt.demo.springbootdemo.entity.User;
 import org.springframework.web.bind.annotation.RestController;
 import com.zyt.demo.springbootdemo.util.Log4j2Util;
+import org.springframework.web.servlet.ModelAndView;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 @RequestMapping("/user")
@@ -21,6 +29,18 @@ public class UserController {
         Log4j2Util.logger.info("查询用户信息\n");
         return userService.findAll();
 
+    }
+
+    private List<User> getUserList(){
+        List<User> lists = userService.findAll();
+        return lists;
+    }
+
+    @GetMapping
+    public ModelAndView userList(Model model) {
+        model.addAttribute("userList",getUserList());
+        model.addAttribute("title", "用户管理");
+        return new ModelAndView("index", "userModel", model);
     }
 
 
